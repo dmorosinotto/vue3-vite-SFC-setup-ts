44b5a78892a409f3521691621f0000b3cc11b1d6
Daniele Morosinotto <d.morosinotto@icloud.com>
2020-09-29 02:37:06 +0200

    Exp7: Vuex module + dynamic load

--- App.vue
+++ App.vue
@@ -1,27 +1,33 @@
 <template>
 	<h3>Vuex! <span v-if="!auth" @click="login">👤</span></h3>
-
+	<!-- COUNTER E' MODULO CARICATO AL VOLO DOPO AVER FATTO IL LOGIN -->
 	<button @click="increment" :disabled="!authenticated">Double {{ count }}</button>
 	<button @click="load()">{{ pending || "LOAD" }}</button>
 	<pre v-if="pending">{{ searchById(1) }}</pre>
+	<pre v-if="authenticated">{{ find(22) }}</pre>
 </template>
 
 <script lang="ts">
 import { computed } from "vue";
-import { useStore, mapMutations, mapGetters, mapActions } from "vuex";
-import type { TMyState } from "./vuex-store";
+import { useStore, mapMutations, mapGetters, mapActions, createNamespacedHelpers } from "vuex";
+import { RootState, moduleCounter } from "./vuex-store";
+
+const { mapGetters: postGetters } = createNamespacedHelpers("posts"); //ESTRAGGO GETTER DI SPECIFICO NAMESPACE
+
 export default {
 	//ESEMPIO UTILIZZO VUEX DA setup = composition API
 	setup() {
-		const store = useStore<TMyState>();
-		const pending = computed(() => store.state.pending);
+		const store = useStore<RootState>();
+		const pending = computed(() => store.state.posts.pending);
 
 		function login() {
 			//QEUSTO E' IL MODO CORRETTO DI INVOCARE mutation -> COMMAND CQRS
 			store.commit("login", true); //ATTENZIONE: mutation E' SEMPRE SINCRONA!!
 			//FUNZIONA ANCHE INVOCAZIONE ALLA redux -> store.commit({type: "login", user: "X", pwd: "X"})
 			//E SI POSSONO ANCHE INVOCARE action -> store.dispatch("LoadPostAsync", [1,2,3])
-			store.dispatch("LoadPostAsync", ["NOT", "FOUND"]); //action E' ASYNCRONA E INTERNAMENTE MODIFICA STATO TRAMITE ctx
+			store.dispatch("posts/LoadPostAsync", ["NOT", "FOUND"]); //action E' ASYNCRONA E INTERNAMENTE MODIFICA STATO TRAMITE ctx
+			//TODO: DOPO LOGIN OK CARICO DINAMICAMENTE MODULO Counter
+			store.registerModule("counter", moduleCounter);
 		}
 		return {
 			login, //ESPONGO metodo -> CHE RICHIAMA mutation
@@ -33,20 +39,22 @@ export default {
 	//ESEMPIO UTILIZZO VUEX CON options API E ACCESSO DIRETTO ALLO STORE
 	computed: {
 		count() {
-			return this.$store.state.count; //ACCESSO DIRETTO A STATE USANDO this.$store
+			return this.$store.state?.counter?.count; //ACCESSO DIRETTO A STATE USANDO this.$store
 		},
-		...mapGetters(["authenticated", "searchById"]) //MA E' MEGLIO ESPORRE DIRETTAMENTE I GETTERS
+		...mapGetters(["authenticated"]), //MA E' MEGLIO ESPORRE DIRETTAMENTE I GETTERS
+		...postGetters(["searchById"]), //ESPONGO IL GETTER DEL NAMESPACE posts/searchById
+		...mapGetters({ find: "posts/searchById" }) //ESPONGO IL GETTER DEL NAMESPACE posts/searchById
 	},
 	methods: {
 		//ESEMPIO DI MODIFICA DELLO STATO VUEX
 		increment() {
 			//BAD PRACTICES: DI SOLITO LO STATO NON VIENE MUTATO DIRETTAMENTE
-			this.$store.state.count += 1;
+			this.$store.state.counter.count += 1;
 			//MA TRAMITE commit mutation -> AKA CQRS COMMAND
-			this.$store.commit("increment");
+			this.$store.commit("counter/increment");
 		},
 		//MEGLIO ANCORA SE ESPONGO DIRETTAMENTE LE ACTIONS (async) CON RIMAPPATURA SU METODI
-		...mapActions({ load: "LoadPostAsync" }) //EQUIVALE A this.$store.dispatch("LoadPostAsync")
+		...mapActions({ load: "posts/LoadPostAsync" }) //EQUIVALE A this.$store.dispatch("posts/LoadPostAsync")
 	}
 };
 </script>
--- vuex-store.ts
+++ vuex-store.ts
@@ -1,68 +1,156 @@
-import { createStore, Store } from "vuex";
+import { createStore, Store, Module } from "vuex";
 
 //ESTRAGGO LA DEFINIZIONE DELLO STATE
 export type inferState<T extends Store<any>> = T extends Store<infer S> ? S : never;
 // export type TS = inferState<typeof vstore>;
-export type TMyState = {
-	count: number;
-	authenticated: boolean;
-	posts: Array<{ id: number; title: string; body: string }>;
-	pending?: string;
-};
 
+//HELPER PER AGGIUNGRE RITARDO CHIAMATA API PER VEDRE IL LOADING...
 export const delay = (ms: number = 2500) => new Promise(resolve => setTimeout(resolve, ms));
 
-export const vstore = createStore<TMyState>({
+//DEFINIZIONE DELLO STATE UTILIZZANDO Module + TRICK TS PER FARE COMPOSIZIONE RootState
+//VEDI DOCS VUEX SULLA ORGANIZZAZIONE MODULI: https://vuex.vuejs.org/guide/modules.html
+export interface RootState {
+	//INTERFACCIA PARTE VUOTA POI USO FEATURE TS OPEN-ENDING X ARRICHIRLA OGNI MODULO CON LA SUA PARTE
+}
+
+/* ******************************************************************************
+ * * *
+ * GESTIONE MODULE Auth (POTREBBE ESSER SU FILE SEPARATO ./modules/Auth.ts)
+ * * *
+ */
+
+export interface AuthState {
+	authenticated: boolean;
+}
+export interface RootState {
+	auth: AuthState;
+}
+
+const moduleAuth: Module<AuthState, RootState> = {
 	//ASSOMIGLIA AL data() DI UN COMPONENTE
 	state() {
 		return {
 			//INITIAL STATE!
-			count: 0,
-			authenticated: false,
-			posts: []
+			authenticated: false
 		};
 	},
 
 	//ASSOMIGLIA A computed DI UN COMPONENTE -> SONO LE QUERY CQRS PER LEGGERE PARTI DELLO STORE
+	namespaced: false, //OSSIA GETTERS E MUTATION ESPOSTE A ROOT
 	getters: {
-		authenticated: state => state.authenticated,
-		loading: state => !!state.pending,
-		searchById: (state, _otherGetters) => (ID: number) => state.posts.find(p => p.id === ID)
+		//PERO DENTRO IL CODICE state E' COMUNQUE QUELLO LOCALE!!
+		authenticated: state => state.authenticated
 	},
 
 	//ASSOMIGLIA A METHODS CON IL COMPITO DI MUTARE LO state -> SONO I COMMAND CQRS PER MODIFICARE
 	mutations: {
-		increment(state) {
-			state.count += 1;
-		},
 		login(state, cred: { user: string; pwd: string } | boolean = false) {
 			if (typeof cred === "object" && cred.user === cred.pwd) {
 				state.authenticated = true;
+				//TODO: DOPO LOGIN OK CARICO DINAMICAMENTE MODULO Counter
+				//vstore.registerModule("counter", moduleCounter); //NON POSSO FARLO QUI AVREI REF-CIRCOLARI VEDI CODICE App.vue
 			} else state.authenticated = !!cred;
-		},
+		}
+	}
+};
+
+/* ******************************************************************************
+ * * *
+ * GESTIONE MODULE Posts (POTREBBE ESSER SU FILE SEPARATO ./modules/Posts.ts)
+ * * *
+ */
+
+export interface PostsState {
+	data: Array<{ id: number; title: string; body: string }>;
+	pending?: string;
+}
+export interface RootState {
+	posts: PostsState;
+}
+
+const modulePosts: Module<PostsState, RootState> = {
+	state() {
+		//INITIAL STATE!
+		return {
+			data: []
+			//pending: undefined
+		};
+	},
+	namespaced: true, //OSSIA GETTERS, MUTATIONE E ACTION SONO SOTTO posts/xxxx E NON A ROOT!
+	getters: {
+		//I getters SONO LE QUERY CQRS PER LEGGERE PARTI DELLO STORE (CHE E' LOCALE)
+		loading: state => !!state.pending,
+		searchById: (state, _otherGetters, _rootState, _rootGetters) => (ID: number) =>
+			state.data.find(p => p.id === ID)
+	},
+	mutations: {
 		//ATTENZIONE: mutation SONO SINCRONE!!!
-		setPost(state, payload: any[]) {
-			state.posts = payload;
+		setPost(state, payload: PostsState["data"]) {
+			state.data = payload;
 		}
 	},
 	//SE SI VUOLE CHIMARE API / OPERAZIONI ASYNCRONE -> USARE actions CHE POI INVOCANO mutation
 	actions: {
 		async LoadPostAsync(ctx, ids?: number | number[]) {
-			if (!ctx.getters.authenticated) return; //POSSO USARE ctx.getters PER ACCEDERE AI GETTERS
-			ctx.state.pending = "LOADING..."; //POSSO USARE ctx.state PER ACCEDERE DIRETTAMENTE STATO
+			if (!ctx.rootGetters.authenticated) return; //POSSO USARE ctx.rootGetters PER ACCEDERE AI GETTERS GLOBALI
+			ctx.state.pending = "LOADING..."; //POSSO USARE ctx.state PER ACCEDERE DIRETTAMENTE STATO LOCALE!
 			try {
 				let res = await window.fetch(`https://jsonplaceholder.typicode.com/posts/${ids || ""}`);
 				await delay(3000); //FAKE DELAY 3s
 				if (res.ok) {
 					let posts = await res.json();
-					//NORMALMENTE SI USA ctx.commit PER INVOCARE LA MUTATION PER ALTARE LO STATO
-					ctx.commit("setPost", posts);
-					//MA VOLENDO POSSO USARE ANCHE ctx.state DIRETTAMENTE
+					//NORMALMENTE SI USA ctx.commit PER INVOCARE LA MUTATION LOCALI!
+					ctx.commit("setPost", posts); //TODO: DI DEFAULT USO LE MUTATION LOCALI AUTO-PREFIX posts/...
+					//ctx.commit("post/setPost", posts, { root: true }); //TODO: VOLENCO POSSO INVARE ANCHE QUELLE GLOBALI
+					//MA VOLENDO POSSO USARE ANCHE ctx.state DIRETTAMENTE PER MODIFICARE STATE LOCALE!
 					ctx.state.pending = "FINISH";
 				} else ctx.state.pending = `ERROR HTTP-${res.status} ${res.statusText}`;
 			} catch (err) {
-				ctx.state.pending = err;
+				ctx.rootState.posts.pending = err; //POSSO USARE ctx.rootState PER ACCEDERE ALLO STATO GLOBALE
 			}
 		}
+	}
+};
+
+/* ******************************************************************************
+ * * *
+ * GESTIONE MODULE Counter (POTREBBE ESSER SU FILE SEPARATO ./modules/Counter.ts)
+ * INOLTRE LO CARICO DINAMICAMENTE AL VOLO AGGANCIANDOLO ALLO STORE DOPO IL login
+ * * *
+ */
+export interface CounterState {
+	count: number;
+}
+export interface RootState {
+	counter?: CounterState;
+}
+
+//TODO: QUESTO MODULO LO CARICO DINAMICAMENTE DOPO IL LOGIN (vedi App.vue)
+export const moduleCounter: Module<CounterState, RootState> = {
+	state() {
+		//INITIAL STATE!
+		return {
+			count: 0
+		};
+	},
+	namespaced: true, //OSSIA MUTATIONE SONO SOTTO counter/xxxx E NON A ROOT!
+	mutations: {
+		increment(state) {
+			state.count += 1;
+		}
+	}
+};
+
+/* ******************************************************************************
+ * * *
+ * ALLA FINE DEFINISCO LO STORE GLOBALE CARICANDO I VARI MODULES
+ * * *
+ */
+
+export const vstore = createStore<RootState>({
+	modules: {
+		//counter: moduleCounter, //TODO: QUESTO VIENE CARICATO DINAMICAMENTE QUANDO FACCIO login
+		auth: moduleAuth,
+		posts: modulePosts
 	}
 });
